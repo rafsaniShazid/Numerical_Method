e67fd28bb1b7149a2753c435846d495e9c9d94c7
Md. Rafsani Shazid <rafsanishazid@gmail.com>
2024-09-01 23:36:12 +0600

    Update Jacobi.cpp

--- Jacobi.cpp
+++ Jacobi.cpp
@@ -1,39 +1,34 @@
-// Jacobi iterative method to solve system of linear equation
 #include<bits/stdc++.h>
 typedef long long ll;
 #define forr(i, j, k) for(ll i = j; i < k; i++)
 using namespace std;
 /*
-Main equations:
-
-27x-6y-z=85.....(1)
-6x+15+2z=72.....(2)
-x+y+54z=110.....(3)
-
+a1x+b1y+c1z=r1
+a2x+b2y+c2z=r2
+a3x+b3y+c3z=r3
 */
-// defining the equations here
-#define f1(y,z) (6*y+z+85)/27 //x
-#define f2(x,z) (-6*x-2*z+72)/15 //y
-#define f3(x,y) (-x-y+110)/54 //z
-
 int main(){
-    float x0=0,y0=0,z0=0,x1,y1,z1,e1,e2,e3,e;
-    // setting up precision after the decimal point    
-    cout<<setprecision(5)<<fixed;
+    cout<<"Enter the variables \n";
+    float a1,b1,c1,a2,b2,c2,a3,b3,c3,r1,r2,r3;
+    float e1,e2,e3,e,x0=0,y0=0,z0=0,x1,y1,z1;
+    cin>>a1>>b1>>c1>>r1;
+    cin>>a2>>b2>>c2>>r2;
+    cin>>a3>>b3>>c3>>r3;
+    cout<<setprecision(6)<<fixed;
+    cout<<"Enter precision\n"; cin>>e;
     int step=1;
-    cout<<"Enter precision \n"; cin>>e;
     do{
-        x1=f1(y0,z0);
-        y1=f2(x0,z0);
-        z1=f3(x0,y0);
-        // checking the errors
-        cout<<step<<"\tx= "<<x1<<", y= "<<y1<<
-        ", z= "<<z1<<"\n";
+        x1= (-b1*y0-c1*z0+r1)/a1;
+        y1=(-a2*x0-c2*z0+r2)/b2;
+        z1= (-a3*x0-b3*y0+r3)/c3;
+        cout<<step<<"\t"<<"x= "<<x1<<", y= "<<y1<<", z= "<<z1<<"\n";
         e1= fabs(x0-x1);
         e2= fabs(y0-y1);
         e3= fabs(z0-z1);
-        x0=x1;y0=y1;z0=z1;
-        step++;
+        x0=x1;
+        y0=y1;
+        z0=z1;
+    step++;
     }while(e1>e && e2>e && e3>e);
     return 0;
 }
